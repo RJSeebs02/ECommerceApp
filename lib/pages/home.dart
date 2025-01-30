249c6c937e56a09cde886cbcc600d0ed460cb7f3
Russ Allen Garde <russgarde03@gmail.com>
2025-01-30 21:43:43 +0800

    helloo

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -51,7 +51,7 @@ class HomePage extends StatelessWidget {
                   fontSize: 20,
                 ),
                 decoration: InputDecoration(
-                  labelText: 'Search',
+                  labelText: 'Searchsssss',
                   labelStyle: const TextStyle(
                     color: AppColors.defaultText,
                     fontSize: 20,
