43130ebe17aad7ff7e2a91656f22397c3dbfce63
RJSeebs02 <zenenix@gmail.com>
2025-02-28 16:48:23 +0800

    Wow

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Flutter\2025 Projects\ecommerceapp"
+export "FLUTTER_ROOT=/Users/qiezera/Development/Flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/qiezera/Development/Flutter/ECommerceApp"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
